3b8aac7ef20497496075eba03c41260e95ec5bdc
samsonkimani <samsonkimani43@gmail.com>
2022-09-08 09:42:16 -0700

    corrected trailing whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-/**
- * main - Entry point 
- *
- *Description: prints '\"Programming is like building a multilingual puzzle'
- *Returns: Always 0 (sucess)
- */
 
-int main(void){
+/**
+ * main - prints Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (sucess)
+**/
+
+
+int main(void)
+{
 	char string[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
-	return 0;
+
+	return (0);
 }
 
