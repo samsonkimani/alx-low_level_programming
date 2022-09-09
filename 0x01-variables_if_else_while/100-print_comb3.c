4f85a8294c774bcc621f62f79f017b94d274f01a
samsonkimani <samsonkimani43@gmail.com>
2022-09-09 10:46:04 -0700

    printing all possible combination of a two digit number

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - printing all th combinations of a two digit numbers without
+ * repetition 01 and 10 are considered a repetition
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (int j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i == '8' && j == '9')
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
