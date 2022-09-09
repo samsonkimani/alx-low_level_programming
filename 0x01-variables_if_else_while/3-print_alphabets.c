4f199e9856612186ca8c2dd1167fabf6d5d443de
samsonkimani <samsonkimani43@gmail.com>
2022-09-09 06:46:37 -0700

    printing all the letters of the alphabet small and capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main - printing all the letters of the alphabet in lower and capital
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+}
+
+
