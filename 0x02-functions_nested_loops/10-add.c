68300d00fdd88befff0bb208953ddfa93cafb0c5
samsonkimani <samsonkimani43@gmail.com>
2022-09-14 03:53:38 -0700

    printing the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - a program that will add two numbers
+ * @a: first parameter
+ * @b: second parameter;
+ *
+ * Description: print the sum of a and b
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
