37c7e1115a65fbe332b2b5e5c0d4d8fdd1db6e71
samsonkimani <samsonkimani43@gmail.com>
2022-09-15 10:34:23 -0700

    creating the mul function that returns the multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplying two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
