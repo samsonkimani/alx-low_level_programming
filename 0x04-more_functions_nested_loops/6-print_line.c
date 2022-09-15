677c6d0c664c5853b71b0a36f4c4df5c372925f1
samsonkimani <samsonkimani43@gmail.com>
2022-09-15 11:44:14 -0700

    printing a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * @n: first parameter
+ */
+
+void print_line(int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+
+}
