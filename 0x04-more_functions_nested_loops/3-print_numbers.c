8f472191eabe4446e8057f80547ff8e2a299eb91
samsonkimani <samsonkimani43@gmail.com>
2022-09-15 10:40:39 -0700

    function print_numbers that prints numbers 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing numbers 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
