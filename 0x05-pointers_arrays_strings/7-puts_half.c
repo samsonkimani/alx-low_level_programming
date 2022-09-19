31da7d1d89b20c04f25168bdf4f355afaafd726e
samsonkimani <samsonkimani43@gmail.com>
2022-09-19 10:03:20 -0700

    printing the last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half the string
+ * @str: main parameter
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; i < (int)(strlen(str)); i++)
+	{
+		if ((int)(strlen(str)) % 2 == 0 && i >= (int)strlen(str) / 2)
+		{
+			_putchar(str[i]);
+		}
+		else if ((int)(strlen(str)) % 2 != 0 && i >= ((int)strlen(str) - 1) / 2)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
