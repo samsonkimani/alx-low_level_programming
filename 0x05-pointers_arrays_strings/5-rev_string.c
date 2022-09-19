87bbd00e28c4583ec23da7e2c6a0d6ade0cb48e7
samsonkimani <samsonkimani43@gmail.com>
2022-09-19 08:51:12 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,19 +10,9 @@ void rev_string(char *s)
 {
 	int i, left, temp;
 
-	left = 0;
-	i = 0;
-
-	while (s[i] != '0')
+	for ((strlen(s) - 1); i >= 0; i--)
 	{
-		i++;
+		_putchar(s[i]);
 	}
-	i--;
-
-	while (i > left)
-	{
-		temp = s[i];
-		s[i--] = s[left];
-		s[left++] = temp;
-	}
+	_putchar('\n');
 }
