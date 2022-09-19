1993dd21e12ef75e7a4e9988a21eab1531ab9520
samsonkimani <samsonkimani43@gmail.com>
2022-09-19 09:03:53 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,12 @@ void rev_string(char *s)
 {
 	int i;
 
+	for (i = 0; i < strlen(s); i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
 	for (i = (strlen(s) - 1); i >= 0; i--)
 	{
 		_putchar(s[i]);
