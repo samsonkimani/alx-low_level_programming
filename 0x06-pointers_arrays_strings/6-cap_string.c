e4033a909e0f394d8129a402464122408931118e
samsonkimani <samsonkimani43@gmail.com>
2022-09-21 12:02:23 -0700

    capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize the beginning of a string
+ * @s: main strin
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				s[i] = s[i] - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						s[i] -= 32;
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
