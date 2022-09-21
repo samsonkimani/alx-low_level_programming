1dadf2f5246f5b1f86990fafb14ae29d22c59432
samsonkimani <samsonkimani43@gmail.com>
2022-09-21 10:42:48 -0700

    converting to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - connvert lowercase to upper
+ * @s: the string to convert
+ *
+ * Return: uppercase
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+}
