3393d5e3c30576924753e2e9df37a6b596eb19bc
samsonkimani <samsonkimani43@gmail.com>
2022-09-26 06:37:01 -0700

    diagonal sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,19 +9,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
+	int i, j, sum1, sum2;
 
+	sum1 = 0;
+	sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (j == i)
 			{
-				sum1 += s[i][j];
+				sum1 += *((a + i * size) + j);
 			}
-			if (j + i == (size - 1))
+			if ((j + i) == (size - 1))
 			{
-				sum2 += s[i][j];
+				sum2 += *((a + i * size) + j);
 			}
 		}
 	}
