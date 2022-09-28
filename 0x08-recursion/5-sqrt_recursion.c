4bda18e27f2e026701b8d9f71a15db9f997c9450
samsonkimani <samsonkimani43@gmail.com>
2022-09-28 11:06:05 -0700

    finding the squareroot of natural numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_square_root - return the squareroot of a number
+ * @i: first input value
+ * @j: second input value
+ *
+ * Returns: the squareroot
+ */
+
+int is_square_root(int i, int j)
+{
+	if (j * j > i)
+	{
+		return (-1);
+	}
+	if (j * j == i)
+	{
+		return (j);
+	}
+	return (is_square_root(i, j + i));
+
+/**
+ * _sqrt_recursion - return the natural squareroot of a number
+ * @n: the main parameter
+ *
+ * Return: the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (is_square_root(n, 1));
+}
