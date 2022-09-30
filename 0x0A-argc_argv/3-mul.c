f7e94b269a0936083e19d11241dda51e59bb1431
samsonkimani <samsonkimani43@gmail.com>
2022-09-30 10:20:02 -0700

    smultiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two arguments
+ * @argc: size of array argument
+ * @argv: the array arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, num1, num2, product;
+
+	if (argc != 3)
+	{
+		return (printf("Error\n"), 1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+	return (0);
+}
