fc0d2742c17492277242b97f3630089cd83ed690
samsonkimani <samsonkimani43@gmail.com>
2022-09-30 06:57:43 -0700

    count arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments
+ * @argc: number of arguments
+ * @argv: the arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int count = 0;
+
+	while (argc--)
+	{
+		argv++;
+		count++;
+	}
+	printf("%d\n", count);
+	return (0);
+}
