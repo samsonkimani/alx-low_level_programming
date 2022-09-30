5a2a58f28688512711e9b1fa8bee1b73f41322f8
samsonkimani <samsonkimani43@gmail.com>
2022-09-30 05:51:53 -0700

    name of the executing file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file
+ * @argc: size of argv
+ * @argv: size of the array
+ *
+ * Return: Always 0
+ */
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
