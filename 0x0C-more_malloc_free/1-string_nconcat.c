dee67be42a67ef19f594258f6bbc98ac6ca2d623
samsonkimani <samsonkimani43@gmail.com>
2022-10-06 11:51:58 -0700

    concatenate n bytes of s2 to s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ar;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	unsigned int m, unsigned int p, unsigned int k = 0;
+	unsigned int m;
+	unsigned int p;
+	unsigned int k = 0;
 	unsigned int len;
 
 	if (s1 == NULL)
