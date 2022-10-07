a4d88f4f7eddc73aaffb2aa4743a314bc39aec46
samsonkimani <samsonkimani43@gmail.com>
2022-10-07 14:09:38 -0700

    multiplacating numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,4 +13,10 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+void _is_zero(char *argv[]);
+
+char *_initialize_array(char *ar, int lar);
+
+int _checknum(char *argv[], int n);
+
 #endif
