1f741050d452338e8f9d1f8dad7dba568405c8a1
samsonkimani <samsonkimani43@gmail.com>
2022-10-11 02:45:02 -0700

    creating a dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * Description: creating a dog structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
