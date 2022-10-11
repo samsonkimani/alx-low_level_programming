033b901da8fa4e025135cd128d8936cd4daac10f
samsonkimani <samsonkimani43@gmail.com>
2022-10-11 06:01:35 -0700

    print the values of struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * print_dog - print all the element in a struct
+ * @d: the structure
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : "(nil)");
+		printf("Owner %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
