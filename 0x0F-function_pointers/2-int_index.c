2efcbf11aa6fe7edcf7cdc4305bc578ddc068636
samsonkimani <samsonkimani43@gmail.com>
2022-10-13 08:19:05 -0700

    compare intger

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,25 +12,22 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, index1;
+	int i;
 
-	if (size > 0)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-			{
-				index1 = i;
-				break;
-			}
-			else
-			{
-				return (-1);
-			}
-		}
-	}
-	else
+	if (size <= 0)
 	{
 		return (-1);
 	}
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] == 1))
+			return (i);
+
+	}
+	return (-1);
 }
