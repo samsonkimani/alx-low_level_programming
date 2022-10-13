342d45b8d7900397061b2b2c5cfacc497eb59134
samsonkimani <samsonkimani43@gmail.com>
2022-10-13 08:28:40 -0700

    check for an inteer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -23,7 +23,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if (cmp(array[i] == 1))
 			return (i);
