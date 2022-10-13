a8be8b11df2139a6d05cfa1f55ffd2301d6f5231
samsonkimani <samsonkimani43@gmail.com>
2022-10-13 05:40:50 -0700

    printing names

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+ * print_name - print a name
+ * @name: the name
+ * @f: pointer to the function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
