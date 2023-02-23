61c92b698eace1cd2e33d98f2ee1f2d098225724
samsonkimani <samsonkimani43@gmail.com>
2023-02-23 15:27:43 +0300

    correcting pycodestyle errors

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" a python function that finds the perimeter of a shape
-surrounded by a water body"""
+""" module - function for perimeter"""
+
 
 def island_perimeter(grid):
+    """ a function to determine the perimeter of a land"""
     one = 1
     count = 0
     for i in range(len(grid)):
         if one in grid[i]:
             for j in range(len(grid[i])):
-                    if grid[i][j] == one:
-                        count += 1
-                    if grid[i][j + 1] != one:
-                        break
+                if grid[i][j] == one:
+                    count += 1
+                if grid[i][j + 1] != one:
+                    break
     perimeter = (count * 2) + 2
     return perimeter
